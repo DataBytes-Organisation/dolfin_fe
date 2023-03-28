a4bfcd73ea7b86ac5a48eae228b51a053e2fd0fc
Samantha Petri <sammypetri@hotmail.com>
2023-03-29 00:03:27 +0800

    adds navbar, homepage & logo

--- frontend/pages/home/home.py
+++ frontend/pages/home/home.py
@@ -3,30 +3,32 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 from pages.home import home_callbacks
 
-layout = dbc.Container([
-    dbc.Row(
-        [
-            html.H6("Overview"),
-            html.Hr(),
-            dbc.Col(
+layout = dbc.Container(
+    [
+    dbc.Col(
             [
-                # dcc.Link(html.Button('Locations'), href=location_page_location),
-                # dcc.Link(html.Button('Optimiser'), href=optimiser_page_location),
-                # dcc.Link(html.Button('Scheduling'), href=schedule_page_location),
-                # dcc.Link(html.Button('Cooke Report'), href=cooke_report_page_location),
+                html.H2("Add borders", className="display-3"),
+                html.Hr(className="my-2"),
+                html.P(
+                    "Or, keep it light and add a border for some added definition "
+                    "to the boundaries of your content."
+                ),
             ],
         ),
-    ],
-    justify="between"
-    ),
-    html.Hr(),
-    dcc.Interval(id='overview-interval-component', interval=120*1000, n_intervals=0),
-    html.Button('Submit', id='home-submit-button', n_clicks=0),
-    dbc.Row(
-        [
-            dbc.Col(html.Div(id='overview-div', children=[]), width=12)
-        ],
-        justify="between",
-    ),
-    html.Div(id='dummy-display', children = [])
-])
+        dbc.Col(
+            [
+                dbc.Carousel(
+                items=[
+                    {"key": "1", "src": "/assets/images/plant.jpg"},
+                    {"key": "2", "src": "/assets/images/pig.jpg"},
+                ],
+                controls=False,
+                indicators=False,
+                interval=2000,
+                ride="carousel",
+                ),
+            ]
+        ),
+        
+    ]
+)
