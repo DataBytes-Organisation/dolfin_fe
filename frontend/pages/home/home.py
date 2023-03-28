f44e9c4556bd54170782cb3ef25425dcb4127814
Samantha Petri <sammypetri@hotmail.com>
2023-03-29 01:39:41 +0800

    adds css styling to homepage

--- frontend/pages/home/home.py
+++ frontend/pages/home/home.py
@@ -5,15 +5,17 @@ from pages.home import home_callbacks
 
 layout = dbc.Container(
     [
+    dbc.Container(
+    [
     dbc.Col(
             [
-                html.H2("Add borders", className="display-3"),
-                html.Hr(className="my-2"),
+                html.H1("JOIN DOLFIN TODAY AND START SAVING!", id="home_header"),
+                html.Hr(),
                 html.P(
-                    "Or, keep it light and add a border for some added definition "
-                    "to the boundaries of your content."
+                    "Simply create an account, link your bank details and begin!",id="home_info"
                 ),
             ],
+            id="home_col1"
         ),
         dbc.Col(
             [
@@ -22,13 +24,27 @@ layout = dbc.Container(
                     {"key": "1", "src": "/assets/images/plant.jpg"},
                     {"key": "2", "src": "/assets/images/pig.jpg"},
                 ],
-                controls=False,
-                indicators=False,
+                controls=True,
+                indicators=True,
                 interval=2000,
                 ride="carousel",
+                id="carousel",
                 ),
-            ]
+            ],
+            id="home_col2",
         ),
         
+    ],
+    id="home",
+),
+dbc.Container(
+    [
+    dbc.Col(
+            [
+            html.Button('JOIN NOW!', id='home-submit-button', n_clicks=0),
+            ]
+    )
     ]
+)
+]
 )
