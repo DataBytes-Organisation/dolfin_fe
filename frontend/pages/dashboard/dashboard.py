b61d249e80dd59a94b021abed337c4f6ddf3aab3
Samantha Petri <sammypetri@hotmail.com>
2023-04-03 22:29:48 +0800

    adds code for pie chart & table on dashboard page

--- frontend/pages/dashboard/dashboard.py
+++ frontend/pages/dashboard/dashboard.py
@@ -1,6 +1,55 @@
 import dash_html_components as html
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
+import plotly.graph_objects as go
 from pages.dashboard import dashboard_callbacks
 
-layout = dbc.Container()
+# TABLE --- ONLY SAMPLE, NEEDS TO BE ALTERED WHEN WE CAN CONNECT TO OUR DATA API
+table = go.Figure(data=[go.Table(
+    header=dict(values=['A Scores', 'B Scores'],
+                line_color='darkslategray',
+                fill_color='lightskyblue',
+                align='left'),
+    cells=dict(values=[[100, 90, 80, 90], # 1st column 
+                       [95, 85, 75, 95]], # 2nd column
+               line_color='darkslategray',
+               fill_color='lightcyan',
+               align='left'))
+])
+
+layout = dbc.Container([
+#PIE CHART
+
+    #---USE THIS ONE ONCE DATA ADDED---
+    # [ 
+    # html.H4("Your Spending Categories"),
+    #     dcc.Graph(id="graph"),
+    #     html.P("Time:"),
+    #     dcc.Dropdown(
+    #         id="times",
+    #         options=["Weekly", "Fortnightly", "Monthly", "Yearly", "All Time"],
+    #         value="times",
+    #         clearable=False,
+    #     ),
+    # ],
+
+
+    #SAMPLE PIE CHART---DELETE THIS ONE ONCE DATA ADDED---
+    html.H4('Analysis of the restaurant sales'),
+        dcc.Graph(id="graph"),
+        html.P("Names:"),
+        dcc.Dropdown(id='names',
+            options=['smoker', 'day', 'time', 'sex'],
+            value='day', clearable=False
+    ),
+    html.P("Values:"),
+        dcc.Dropdown(id='values',
+            options=['total_bill', 'tip', 'size'],
+            value='total_bill', clearable=False
+    ),
+
+    ],
+
+    dashboard_callbacks,
+    table,
+)
--- frontend/pages/dashboard/dashboard_callbacks.py
+++ frontend/pages/dashboard/dashboard_callbacks.py
@@ -1,3 +1,20 @@
 from classes.api import API
 from dash.dependencies import Input, Output, State
 from app import app
+from classes.api import API
+from dash.dependencies import Input, Output, State
+from app import app
+import plotly.express as px
+
+
+@app.callback(
+    Output("graph", "figure"), 
+    Input("names", "value"), #CHANGE NAMES TO TIMES
+    Input("values", "value"))
+def generate_chart(names, values): #CHANGE NAMES TO TIMES
+    df = px.data.tips() #REPLACE WITH OUR DATA
+    fig = px.pie(df, values=values, names=names, hole=.3) #CHANGE NAMES TO TIMES
+    return fig
+
+
+app.run_server(debug=True)
