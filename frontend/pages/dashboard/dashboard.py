c0a8b61ae84d35d62e92bcbefec39f8e48187d71
joshphuocdang <dangtha@deakin.edu.au>
2023-04-05 14:07:59 +1000

    add dashboard page

--- frontend/pages/dashboard/dashboard.py
+++ frontend/pages/dashboard/dashboard.py
@@ -1,6 +1,50 @@
-import dash_html_components as html
-import dash_core_components as dcc
+import dash
+from dash import dcc, html
 import dash_bootstrap_components as dbc
 from pages.dashboard import dashboard_callbacks
+import pandas as pd
+import dash_table
 
-layout = dbc.Container()
+
+df = pd.read_csv('pages/dashboard/dummies.csv')
+
+layout = dbc.Container([
+
+    dbc.Row([
+        html.H1("YOUR SPENDING BREAKDOWN",
+                        className='text-center text-primary mb-4'),  
+
+        html.H3("MONTHLY SPENDING",
+                        className='text-center text-primary mb-4'),
+ 
+        dbc.Row([
+        
+                dbc.Col(html.P('Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris\
+                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in\
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia\
+                        deserunt mollit anim id est laborum.', className='text-right text-primary, mb-4'),
+                    width = {'size': 3,'offset' : 0}),
+                dbc.Col([
+                        dcc.Dropdown(id = 'piechart_dd', multi = False, placeholder = 'Select a year',
+                        options = [{'label':x, 'value':x} for x in sorted(df.iloc[:,3].unique())]),
+            
+                        dcc.Graph(id = 'piechart', figure={})
+                ])
+            ]
+        ),
+    
+        dbc.Row(
+            dash_table.DataTable(
+                data = df.to_dict('records'), 
+                columns = [{"name": i, "id": i} for i in df.columns],
+                style_cell = {'textAlign':'left'},
+                    style_header={
+                    'backgroundColor': '#4f8fe3',
+                    'fontWeight': 'bold'
+                    }
+                ))
+    ])
+])
--- frontend/pages/dashboard/dashboard_callbacks.py
+++ frontend/pages/dashboard/dashboard_callbacks.py
@@ -1,3 +1,24 @@
 from classes.api import API
 from dash.dependencies import Input, Output, State
 from app import app
+import pandas as pd
+import plotly
+import plotly.express as px
+
+df = pd.read_csv('pages/dashboard/dummies.csv')
+
+
+@app.callback(
+    Output('piechart', 'figure'),
+     Input('piechart_dd', 'value')
+)
+def update_graph(year):
+    dff = df[df.iloc[:,3]==year]
+    fig_pie = px.pie(
+        data_frame = dff,
+        names = "Categories",
+        values = 'Amount',
+        hole = .3,
+        labels = 'Categories'
+    )
+    return fig_pie
