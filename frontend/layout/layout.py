40e215ea50f0a634184f3000499b7dfc6afb99f3
Muhammad Sarim <62735622+msarim010@users.noreply.github.com>
2023-04-24 07:39:45 +0000

    Changing Layout to adjust Footer

--- frontend/layout/layout.py
+++ frontend/layout/layout.py
@@ -2,9 +2,19 @@ import dash_html_components as html
 import dash_core_components as dcc
 
 from layout.sidebar.sidebar import sidebar
-from layout.navbar.navbar import navbar,logo
+from layout.navbar.navbar import navbar, logo
+from components.footer import Footer  # Import the Footer component
 
+content = html.Div(id="page-content", className="content-container")
 
-content = html.Div(id="page-content")
+# Wrap the content, sidebar, and footer inside a new div with the "page-container" class
+page_container = html.Div(
+    [
+        sidebar,
+        content,
+        Footer(),  # Add the Footer component
+    ],
+    className="page-container",
+)
 
-layout = html.Div([dcc.Location(id="url"),navbar, logo, sidebar, content])
+layout = html.Div([dcc.Location(id="url"), navbar, logo, page_container])
