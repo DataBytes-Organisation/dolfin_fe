3f8ddb413e492b970c697bde37a4d3f63dfc6dc9
Dan <danielpnorris264@outlook.com>
2023-05-02 07:44:36 +1000

    working on chages, swapping out csv

--- frontend/classes/api.py
+++ frontend/classes/api.py
@@ -53,26 +53,47 @@ class API:
         except Exception as e:
             raise Exception(f"Sign in for user : {email} failed")
 
-    # def dashboard(self) -> dict:
-    #     try:
+    def dashboard(self) -> dict:
+        try:
     
-    #         url = self.base_url+"/basiq/transactions"
+            url = self.base_url+"/basiq/transactions"
 
-    #         payload = {}
-    #         headers = {
-    #             "Authorization": "eyJraWQiOiJRRU5nQ0V4OVwvcGp4NnhsQXZrYnVcL0RrN1VKc0lDQm5Zend5VHd2THVzdW89IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiIwOTY0NTU5Ny1jNWQ1LTQwZjUtOTljZi1mYjg1NGYyMTU5ZTMiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGhlYXN0LTIuYW1hem9uYXdzLmNvbVwvYXAtc291dGhlYXN0LTJfQXRoaWVpeWphIiwiY29nbml0bzp1c2VybmFtZSI6IjA5NjQ1NTk3LWM1ZDUtNDBmNS05OWNmLWZiODU0ZjIxNTllMyIsIm9yaWdpbl9qdGkiOiIyNjNjZWNkNy01ZWI0LTQyYzctOTQxZS1jYmYxZDQyMjU3ZTYiLCJhdWQiOiIxOTBlMjFtcTg0amtyMjM2bzQ3ZTBpY3JubSIsImV2ZW50X2lkIjoiMmYxNzRmYWYtMjUzMy00YmFhLTk5ZjItNmY0Nzk2YTNmOGRkIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE2ODI4NDMxOTMsIm5hbWUiOiJmYWtlZGFuZmFrZW5vciIsImV4cCI6MTY4Mjg0Njc5MywiaWF0IjoxNjgyODQzMTkzLCJqdGkiOiJiY2ZmM2U5Ny0xY2MxLTRmYjYtYTFlMi0xMTgzNjEzMTYyZDciLCJlbWFpbCI6ImZha2VkYW5ub3JAZ21tYWFpbC5jb20ifQ.sa1tGQAbDYBeq35hdmR2h8MR9tCoG293AuJLVEt7P0gzSDZwrSYGeAELA1BbzEG3p0_e3zjjMIe7WxRKaoEtzJ78vpfCcekjDDhSZ9-bZl7msUhDFIQyomAxsddmnRUkqfUlYEbZDoEderKcQzQvxMrsRuy8Z7nDKNBRoN9Bv8KBUGCgU8dXIX7FfELIM6LVPzPgzEtE_M6_e4gUKeUDi-4zPwOPb_1I2sEKnxDZCCFn7m2V7q35a0YlRXKI93d0-0H7NhOX0BnOW6fOuy9n8CiXCqhu_ynOLFV27OpPWZpT9L7-ydLHAsevFqNFwWFK3lLhQhZHc3sDJ1LEX4lmng"
-    #         }    
+            payload = {}
+            headers = {
+                "accepts": "application/json",
+                "Authorization": "eyJraWQiOiJOT0RocDQ3NVNVOWxZN3IzdkcxdU5PbW81bGVuSmFsZ3FFb3lFZ1g2TmJZPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJlN2IzMDkwOS1mY2Q5LTRkYjctYWY0ZS0zMTIxZjBjMjc3NWYiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGhlYXN0LTIuYW1hem9uYXdzLmNvbVwvYXAtc291dGhlYXN0LTJfMFRDak1zODluIiwiY29nbml0bzp1c2VybmFtZSI6ImU3YjMwOTA5LWZjZDktNGRiNy1hZjRlLTMxMjFmMGMyNzc1ZiIsIm9yaWdpbl9qdGkiOiI0Zjg2OGZlMy01YWZkLTRiZGYtOWRmNi1lYzEwYmI4YzE4MjciLCJhdWQiOiJrajgybXBmaG0wZWd1MTNmM2w5bTEyNWs1IiwiZXZlbnRfaWQiOiI1NDdmMjE0Zi0xNGFjLTQwZDYtOGQ2Mi04MGRkMTBkM2NlMTgiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTY4Mjk1MDkyNiwibmFtZSI6ImZha2VmaXJzdGZha2VsYXN0IiwiZXhwIjoxNjgyOTU0NTI2LCJpYXQiOjE2ODI5NTA5MjYsImp0aSI6IjBkMDllZTM0LTY2NTAtNDIxYi05YzdkLThhZTIwMjJiNTYzOCIsImVtYWlsIjoieW91ci1lbWFpbEB0ZXN0LnRlc3R0In0.ItwuuxSw2YCtR-bff7EXgFhWXSJ4jnhl_sJAu_rFZJzrD5sdIaJ8sZkHSrSe87Go5RVQQNSW7bgI6lEd7Y_1605lyUPN79FF7-RwGEJKe1oFuRGMbIrrEU7yVs0NGh2CxKevKeNqUkySJsepIqU4kjLPYbTkB7TzkLa-27oF0l3zt7KPsm6S7Aky8pO6v_K6AT9Zeovk4Q_-yd1TzLdhl4k6y_tm6_ogBT-5vOu_UsCY8jECIKtagEY9XsCsZeG9yuENwr4rsy8Zq7FK0ZPz8KOBLgysF8dY_S6SR_IGu5OxzKlqyhkJO2UIl56F6NfCPcy2tGFFAvkLgE9wSOd2vw"
+                }    
 
-    #         response = requests.request("GET", url, headers=headers, data=payload)
-    #         if response.status_code > 299:
-    #             raise Exception("HTTP {response.statuscode} Bad Status Code")
-    #         #print(response.json())
-    #         return json.loads(response.text)
+            response = requests.request("GET", url, headers=headers, data=payload)
+            if response.status_code > 299:
+                raise Exception("HTTP {response.statuscode} Bad Status Code")
+            #print(response.json())
+            return json.loads(response.text)
         
-    #     except Exception as e:
-    #         raise Exception("Error getting dashboard data failed")
+        except Exception as e:
+            raise Exception("Error getting dashboard data failed")
+
+    def get_accounts(self) -> dict:
+        try:
+    
+            url = self.base_url+"/basiq/accounts"
+
+            payload = {}
+            headers = {
+                "accepts": "application/json",
+                "Authorization": "eyJraWQiOiJOT0RocDQ3NVNVOWxZN3IzdkcxdU5PbW81bGVuSmFsZ3FFb3lFZ1g2TmJZPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJlN2IzMDkwOS1mY2Q5LTRkYjctYWY0ZS0zMTIxZjBjMjc3NWYiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGhlYXN0LTIuYW1hem9uYXdzLmNvbVwvYXAtc291dGhlYXN0LTJfMFRDak1zODluIiwiY29nbml0bzp1c2VybmFtZSI6ImU3YjMwOTA5LWZjZDktNGRiNy1hZjRlLTMxMjFmMGMyNzc1ZiIsIm9yaWdpbl9qdGkiOiJjYTg2Y2U0Yy1kNDc2LTQ0MjktYmYzNi0xMDMzYzkyNmM5YmMiLCJhdWQiOiJrajgybXBmaG0wZWd1MTNmM2w5bTEyNWs1IiwiZXZlbnRfaWQiOiIxNzdhYjhiOC05MDk4LTQ0M2YtOGNkZi0xYzUwZGVjMWIxZDgiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTY4Mjk0NzI1NywibmFtZSI6ImZha2VmaXJzdGZha2VsYXN0IiwiZXhwIjoxNjgyOTUwODU3LCJpYXQiOjE2ODI5NDcyNTcsImp0aSI6IjIwNWIyOWIwLWUwYjEtNGNlNC1hMTBjLTRlYzBkODhiNGQyNCIsImVtYWlsIjoieW91ci1lbWFpbEB0ZXN0LnRlc3R0In0.BTSimgeR5uBf9vyJXArW9aqzLNMWGy8z71vSx5W2fMalq-iUwRKYERYIZjjc7TY7UMeMy0vVL9eBNtkjddNCmEYIVgH-xZqLy8sSnUwLQc4QnxwIRFxQJy379po3o3xL7_EKKspENTHAcJXSKDqjuFOy8RRngyfGJ7BqO3JY2fimvE3W4saBPi12-yKFUb6Cvb1TX1PL7EoVjO33VB2dK1zwoV2OH5676arAR16xZzffivdwfGemet_hTljQyraY97qmOsjKOu4ddBjC_JUA8n4GDcYylIgKCHHNMlihn1t0B29bbQgk2i_0voqaJi26ZmBLAlWyi5GYlbmjqSzwRA"
+                }    
+
+            response = requests.request("GET", url, headers=headers, data=payload)
+            if response.status_code > 299:
+                raise Exception("HTTP {response.statuscode} Bad Status Code")
+            #print(response.json())
+            return json.loads(response.text)
         
-# class BE_API:
-#     def __init__(self) -> None:
-#         self.backend_api_url = backend_api_url
-#         print(self.backend_api_url)
+        except Exception as e:
+            raise Exception("Error getting dashboard data failed")
+        
+class BE_API:
+    def __init__(self) -> None:
+        self.backend_api_url = backend_api_url
+        print(self.backend_api_url)
--- frontend/pages/dashboard/dashboard.py
+++ frontend/pages/dashboard/dashboard.py
@@ -4,16 +4,27 @@ import dash_bootstrap_components as dbc
 from pages.dashboard import dashboard_callbacks
 import pandas as pd
 import dash_table
+import json
+
+df = dashboard_callbacks.get_transaction_data()
+accounts = dashboard_callbacks.get_accounts()
+
+# df = pd.json_normalize(test)
+accounts_df = pd.json_normalize(accounts)
 
 
-df = pd.read_csv('pages/dashboard/processed_user_outgoing_transaction_data.csv')
+
+# df = pd.read_csv('pages/dashboard/processed_user_outgoing_transaction_data.csv')
 columns = [{"name": i, "id": i} for i in df.columns]
-columns[3]["name"] = "Amount"
-columns[7]["name"] = "Category"
-columns[8]["name"] = "Year"
-columns[9]["name"] = "Month"
+columns[2]["name"] = "Amount"
+columns[6]["name"] = "Category"
+columns[7]["name"] = "Year"
+columns[8]["name"] = "Month"
 
-# test = pd.read_json(dashboard_callbacks.get_transaction_data())
+test = dashboard_callbacks.get_transaction_data()
+# test2 = json.loads(test)
+
+# df = pd.json_normalize(test)
 
 layout = dbc.Container([
 
@@ -23,16 +34,14 @@ layout = dbc.Container([
 
         html.H3("YEARLY SPENDING CATEGORIES",
                         className='text-center text-primary mb-4'),
-        
-        # html.H1(test[0]),
- 
+        # html.H1(df),
         dbc.Row([
         
                 dbc.Col(html.P('Chart shows categories of spending over the selected year.', className='text-right text-primary, mb-4'),
                     width = {'size': 3,'offset' : 0}),
                 dbc.Col([
                         dcc.Dropdown(id = 'piechart_dd', multi = False, placeholder = 'Select a year',
-                        options = [{'label':x, 'value':x} for x in sorted(df.iloc[:,8].unique())]),
+                        options = [{'label':x, 'value':x} for x in sorted(df.iloc[:,7].unique())]),
             
                         dcc.Graph(id = 'piechart', figure={})
                 ])
