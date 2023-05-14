a060a2d685e3fb2892288e653fb4fd2b265d0456
Dan <danielpnorris264@outlook.com>
2023-05-14 10:07:01 +1000

    Dashboard changes

--- frontend/classes/api.py
+++ frontend/classes/api.py
@@ -64,5 +64,23 @@ class API:
         except Exception as e:
             raise Exception(f"There was an issue getting a refresh token")
 
+    def dashboard(self) -> dict:
+        try:
     
+            url = self.base_url+"/basiq/transactions"
+
+            payload = {}
+            headers = {
+                "accepts": "application/json",
+                "Authorization": "eyJraWQiOiJcL1pZTmRxczQwRFVHYjE2MFAyNThTVzJnYW0zUXhlSkZMeVlpb3JFck80Zz0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJiMTVlZmQwZS05YjY2LTRjNDctYjM0My1hZTc1YmRmMGI1NTQiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGhlYXN0LTIuYW1hem9uYXdzLmNvbVwvYXAtc291dGhlYXN0LTJfZXZNMHd6ZTRqIiwiY29nbml0bzp1c2VybmFtZSI6ImIxNWVmZDBlLTliNjYtNGM0Ny1iMzQzLWFlNzViZGYwYjU1NCIsIm9yaWdpbl9qdGkiOiJhOGE3ZTUzNC1mMmE1LTQ5YTItYWQ4ZS0zN2MwNGY3ZGVkYTYiLCJhdWQiOiIzOHBwbG1zb2p1ZHNjMDhzZ2wwdWQ0djdqciIsImV2ZW50X2lkIjoiNjIyM2YzODAtZGQ0YS00ZWM5LTgyZmQtM2U4YmMyYmY1MjhiIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE2ODQwMjI2MjUsIm5hbWUiOiJkYW5ubm9yciIsImV4cCI6MTY4NDAyNjIyNSwiaWF0IjoxNjg0MDIyNjI1LCJqdGkiOiI4NGM4ZDk5Yy0xMjlhLTQyNGYtODZiYS1jNmU0MjAxM2RiNmQiLCJlbWFpbCI6ImRhbkBlbWFpbC5jb21tIn0.uM5Jq3-PiT1U_xFph-JFsFDBMvPD7GG9WlfBDP955iTFbzaM9mSnKDVWAqvXkQ6Gr2HkvH9qaVts2R3w1S3dug8sr0iSBmZy7fDBkbE-_OmD4m_LshRF1DqW7jVmmEMF-OyL_s3H-P_VYTawni924pCSSZ7rJ1qIXMXsgxpFKOrC7DwXXAtC_HKGzWGJZu9_JHy7cHseweYOXm9T3pNNMjtdI8BrlYF785jVFVoi6AU0wzx9os7p6xryG9nn4ar6f9ojnpN-gesZt9CxXem-5jO4GvQkGhluDq1ukd0ad6cHBpCe2QwP-yBMNfRQSiW-iCkGoVgOLHDvI1tB3fFjaA"
+                }    
+
+            response = requests.request("GET", url, headers=headers, data=payload)
+            if response.status_code > 299:
+                raise Exception("HTTP {response.statuscode} Bad Status Code")
+            #print(response.json())
+            return json.loads(response.text)
         
+        except Exception as e:
+            raise Exception("Error getting dashboard data failed")
+       
--- frontend/pages/dashboard/dashboard_callbacks.py
+++ frontend/pages/dashboard/dashboard_callbacks.py
@@ -4,21 +4,99 @@ from app import app
 import pandas as pd
 import plotly
 import plotly.express as px
+import json
 
-df = pd.read_csv('pages/dashboard/dummies.csv')
-
+dfff = pd.read_csv('pages/dashboard/processed_user_outgoing_transaction_data.csv')
 
 @app.callback(
     Output('piechart', 'figure'),
      Input('piechart_dd', 'value')
 )
 def update_graph(year):
-    dff = df[df.iloc[:,3]==year]
+    df = list_outgoing_transactions()
+    dff = dfff[dfff.iloc[:,8]==year]
     fig_pie = px.pie(
         data_frame = dff,
-        names = "Categories",
-        values = 'Amount',
+        names = "category",
+        values = 'amount',
         hole = .3,
-        labels = 'Categories'
+        labels = 'category'
     )
     return fig_pie
+
+def get_transaction_data():
+    api = API()
+    transaction_result = api.dashboard()
+    data = json.dumps(transaction_result)
+    data2 = json.loads(data)
+
+    accounts = get_accounts()
+
+    df = pd.json_normalize(data2)
+    accounts_df = pd.json_normalize(accounts)
+
+    processedData = df.filter(["id", "status", "amount", "direction", "account", "class", "postDate", "subClass.title"], axis=1)
+    monthsForProcessing = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+    #Renamed 'subClass.title' as 'category'. This is not the category I was looking to use but it could do the job
+    processedData.rename(columns = {'subClass.title':'category'}, inplace = True)
+    return processedData
+    #Processing the date value that is returned. It will be split from a string in to 3 arrays listed below
+    year = []
+    month = []
+    day = []
+
+    #Account name included to give something human readable to the account id number
+    accountName = []
+
+    #Here I am transforming the data in the postDate and account columns. 
+    for column in df[["postDate", "account"]]:
+        for val in df[column]:
+            if column == "postDate":
+                
+                #postDate has the initial string value split, giving us a date formatted to yyy-mm-dd at the 0th index. This is then
+                #split at the '-' into separate years, months and dates and added in to the appropriate array above 
+                
+                initialSplit = val.split('T')
+                yearMonthDay = initialSplit[0].split("-")
+                year.append(yearMonthDay[0])
+                month.append(monthsForProcessing[int(yearMonthDay[1]) - 1])
+                day.append(yearMonthDay[2])
+            if column == "account":
+                
+                #If the account id matches the account id in the 'Accounts' dataframe (created 2 cells above) then the account name
+                #is added to the accountName array, to be added in to the dataframe 
+                for index, accountNumber in enumerate(accounts_df["id"]):
+                    if val == accountNumber:
+                        accountName.append(accounts_df["name"][index])
+                        break
+
+    #All of the new values are added to the existing dataframe
+    processedData["year"] = year
+    processedData["month"] = month
+    processedData["day"] = day
+    # processedData["accountName"] = accountName
+    processedData = processedData.drop('postDate', axis=1)
+    processedData["category"].fillna("Unknown", inplace = True)
+
+    return processedData
+
+def get_accounts():
+    api = API()
+    transaction_result = api.dashboard()
+    data = json.dumps(transaction_result)
+    data2 = json.loads(data)
+    return data2
+
+def list_outgoing_transactions():
+    initial_transactions = get_transaction_data()
+            
+    outgoing_transactions = initial_transactions[initial_transactions.amount.astype('float64') < 0]
+
+    #transforming values to absolute value to prevent the issue mentioned above
+    for index, row in outgoing_transactions.iterrows():
+        row["amount"] = abs(float(row["amount"]))
+        
+    monthlySpending = outgoing_transactions.groupby(['year', 'month']).sum().filter(['year', 'month', 'amount'], axis=1)
+
+    return monthlySpending
